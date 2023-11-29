f57af230ea63460c0c7e65bbbead6d3d182972a0
SvitRia <129522432+SvitRia@users.noreply.github.com>
2023-11-29 19:51:38 +0000

    Deploying to gh-pages from @ SvitRia/goit-react-hw-08-phonebook@c2a2792136233dfbba5907cc816ccbd4a4b66607 🚀

---
+++ static/js/845.a241f071.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[845],{9845:function(e,t,n){n.r(t),n.d(t,{default:function(){return r}});var c=n(184),i={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function r(){return(0,c.jsx)("div",{style:i.container,children:(0,c.jsx)("h1",{style:i.title,children:"Your contact's book"})})}}}]);
+//# sourceMappingURL=845.a241f071.chunk.js.map
